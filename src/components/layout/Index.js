e06f4d3f60f49868004f66175d9b97fc192e779d
Rafael <136873880+RegulusRK@users.noreply.github.com>
2025-05-30 21:27:02 -0300

    add: context and tracks files

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Tracks from '../tracks/Tracks'
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <React.Fragment>
+        <Tracks/>
+    </React.Fragment>
   )
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const navbar = () => {
   return (
     <nav className='navbar navbar-dark bg-dark mb-5'> 
-    <span className='navbar-brand mb-0 h1 mx-auto'>Lyric Nest</span>
+    <span className='navbar-brand mb-0 h1 mx-auto'>Lyrics Nest</span>
 
   </nav>)
 }
